2b89b0dd1f04146a254f3bf1a02db5021942697b
AE <ae@devuan.ae>
2023-08-22 15:55:00 -0400

    GNU Argp support added

--- main.cpp
+++ main.cpp
@@ -11,58 +11,57 @@
 namespace ra = std::ranges;
 namespace rv = ra::views;
 
-void print(ra::viewable_range auto &&v, std::string_view separator = ", ") {
-    std::cout << '[';
-    for (const auto &x : v) {
-        for (const auto &e: x)
-            std::cout << e << separator;
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    if (key == 'i') {
+        *(std::string *)state->input = arg;
     }
-    std::cout << ']' << '\n';
+    return 0;
 }
 
 auto main(int argc, char **argv) -> int {
-    argp_parse(0, argc, argv, 0, 0, 0);
+    std::string filename;
+    static const argp_option options[] = {
+        {"input", 'i', "FILE", 0, "Input file", 0},
+        {nullptr, 0, nullptr, 0, nullptr, 0}};  // -Werror=missing-field-initializers
+    static const argp argp_parser = {
+        options, parse_opt, "FILE", "Pass a file as an argument using -i flag",
+        nullptr, nullptr, nullptr};             // -Werror=missing-field-initializers
+    argp_parse(&argp_parser, argc, argv, 0, 0, &filename);
 
-    std::ifstream file("example.txt"); // Open your file here
+    std::ifstream file(filename);
 
     if (!file.is_open()) {
         std::cerr << "Failed to open the file." << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    const std::vector labyrinth = [&]() {
-        std::vector<std::string> result;
-        for (std::string line; std::getline(file, line); )
+    const auto labyrinth = [&]() {
+        std::vector<std::string> result;    // ITM anti-pattern
+        for (std::string line; std::getline(file, line);)
             result.push_back(line);
         return result;
     }();
 
-    std::vector<std::vector<int>> matrix;
-
-    for (const std::string& row : labyrinth) {
-        std::vector<int> values;
-        for (std::size_t i = 0; i < row.size(); ++i) {
-            if (row[i] == ' ') values.push_back(0);
-            else {
-                values.push_back(1);
-                i += 2; // Skip the next two characters (weird bug where █ counts as 3 characters)
-            }
+    const auto matrix = [&]() {
+        std::vector<std::vector<int>> result;   // ITM anti-pattern
+        for (const auto& row : labyrinth) {
+            std::vector<int> values;            // ITM anti-pattern
+            for (std::size_t i = 0; i < row.size(); i += (row[i] == ' ' ? 1 : 3)) // Skip the next two characters
+                values.push_back(row[i] == ' ' ? 0 : 1);                          // (weird bug where █ counts as 3 characters)
+            result.push_back(values);
         }
-        matrix.push_back(values);
-    }
+        return result;
+    }();
 
     for (const auto &s : labyrinth)
         std::cout << s << '\n';
 
-    // print(matrix);
     for (const auto& row : matrix) {
-        for (int val : row) {
+        for (int val : row)
             std::cout << val << " ";
-        }
         std::cout << "\n";
     }
 
     file.close();
-
     exit(EXIT_SUCCESS);
 }
--- makefile
+++ makefile
@@ -5,5 +5,9 @@ MAKEFLAGS += --silent
 FILENAME = main
 
 default:
+	$(CXX) $(CFLAGS) $(CFLAGS2) $(FILENAME).cpp -o $(FILENAME)
+	./$(FILENAME) -i example.txt
+run:
 	$(CXX) $(CFLAGS) $(FILENAME).cpp -o $(FILENAME)
-	./$(FILENAME)
+	./$(FILENAME) -i example.txt
+	rm $(FILENAME)
