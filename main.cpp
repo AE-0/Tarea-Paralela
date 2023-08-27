29d33702bd0ce3a895b75b54f705c76f05e4116e
AE <ae@devuan.ae>
2023-08-27 19:54:07 -0400

    Flood fill algorithm + some small fixes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <numeric>
 #include <ranges>
 #include <array>
+#include <queue>
 #include <string>
 #include <fstream>
 #include <iostream>
@@ -31,12 +32,57 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     return 0;
 }
 
+template <typename T>
+void print(const std::vector<T> &v, std::string_view separator = " ") {
+    for (const auto& e : v)
+        std::cout << e << separator << '\n';
+}
+
+template <typename T>
+void print(const std::vector<std::vector<T>> &m, std::string_view separator = " ") {
+    for (const auto& v : m) {
+        for (const auto& e : v)
+            std::cout << e << separator;
+        std::cout << '\n';
+    }
+}
+
+auto flood_fill(std::vector<std::vector<int>> maze, std::size_t start_row, std::size_t start_col) -> std::vector<std::vector<int>> {
+    const auto rows = maze.size();
+    const auto cols = maze[0].size();
+
+    std::vector<std::vector<int>> solved_maze(rows, std::vector<int>(cols, -1));  // Initialize matrix with -1
+    solved_maze[start_row][start_col] = 0;  // Starting point
+
+    std::queue<std::pair<int, int>> q;
+    q.push({start_row, start_col});
+
+    while (!q.empty()) {
+        auto [cur_row, cur_col] = q.front();  // C++17 Structured bindings
+        q.pop();
+
+        const std::vector dr = {-1, 1, 0, 0};
+        const std::vector dc = {0, 0, -1, 1};
+
+        for (const auto& i : rv::iota(0, 4)) { // C++20 std::ranges::views::iota
+            std::size_t new_row = cur_row + dr[i];
+            std::size_t new_col = cur_col + dc[i];
+
+            if (new_row < rows && new_col < cols && maze[new_row][new_col] == 0 && solved_maze[new_row][new_col] == -1) {
+                solved_maze[new_row][new_col] = solved_maze[cur_row][cur_col] + 1;
+                q.push({new_row, new_col});
+            }
+        }
+    }
+    return solved_maze;
+}
+
 void encode(std::vector<std::vector<int>> matrix, std::string wall, std::string space, std::string ouput_file) {
         std::ofstream file;
         file.open(ouput_file);
         for (const auto& row : matrix) {
-            for (std::size_t i = 0; i < row.size(); i ++)
-                file << (row[i]? wall : space );
+            for (const auto& elem : row)
+                file << (elem ? wall : space);
              file << '\n';
         }
         file.close();
@@ -48,31 +94,29 @@ auto main(int argc, char **argv) -> int {
     static const argp_option options[] = {
         {"input", 'i', "FILE", 0, "Input file", 0},
         {"output", 'o', "FILE", 0, "Output file", 0},
-        {nullptr, 0, nullptr, 0, nullptr, 0}};  // -Werror=missing-field-initializers
+        {nullptr, 0, nullptr, 0, nullptr, 0}};  // supresses -Werror=missing-field-initializers
     static const argp argp_parser = {
         options, parse_opt, "FILE", "Pass a file as an argument using -i and -o flag",
         nullptr, nullptr, nullptr};             // -Werror=missing-field-initializers
     argp_parse(&argp_parser, argc, argv, 0, 0, &args);
 
     std::ifstream file(args.input_filename);
-
-    std::cout << args.output_filename;
-
     if (!file.is_open()) {
-        std::cerr << "Failed to open the file." << std::endl;
+        std::cerr << args.output_filename << ": No such file or directory." << '\n';
         exit(EXIT_FAILURE);
     }
 
-    const auto labyrinth = [&]() {
+    const auto maze = [&]() {          // Immediately invoked lambda (IIFE) improves performance and inmutability
         std::vector<std::string> result;    // ITM anti-pattern
         for (std::string line; std::getline(file, line);)
             result.push_back(line);
+        result.pop_back();                  // Removes null character, avoiding segfault when converting to integer
         return result;
     }();
 
-    const auto matrix = [&]() {
+    const auto matrix = [=]() {
         std::vector<std::vector<int>> result;   // ITM anti-pattern
-        for (const auto& row : labyrinth) {
+        for (const auto& row : maze) {
             std::vector<int> values;            // ITM anti-pattern
             for (std::size_t i = 0; i < row.size(); i += (row[i] == ' ' ? 1 : 3)) // Skip the next two characters
                 values.push_back(row[i] == ' ' ? 0 : 1);                          // (weird bug where █ counts as 3 characters)
@@ -81,15 +125,16 @@ auto main(int argc, char **argv) -> int {
         return result;
     }();
 
+    const auto start = ra::find_if(matrix, [](const auto& e) { // pointer to the first 0 value element
+        return ra::find(e, 0) != e.end();
+    });
+    const auto start_row = std::distance(matrix.begin(), start);
+    const auto start_column = std::distance(start->begin(), ra::find(*start, 0));
 
-    for (const auto &s : labyrinth)
-        std::cout << s << '\n';
+    const auto solved_maze = flood_fill(matrix, start_row, start_column);
 
-    for (const auto& row : matrix) {
-        for (int val : row)
-            std::cout << val << " ";
-        std::cout << "\n";
-    }
+    print(maze, "");
+    print(solved_maze);
 
     encode(matrix, "█", " ", args.output_filename);
 
--- makefile
+++ makefile
@@ -1,13 +1,17 @@
-CFLAGS = -std=c++2b -O2
-CFLAGS2 = -Wall -Wextra -Wpedantic -Wconversion -Werror -g
+CXX_FLAGS = -std=c++2b -O2
+CXX_FLAGS2 = -Wall -Wextra -Wpedantic -Wconversion -Werror -g
+CXX_FLAGS3 = -std=c++2b -ggdb
 CXX = g++
 MAKEFLAGS += --silent
 FILENAME = main
 
 default:
-	$(CXX) $(CFLAGS) $(CFLAGS2) $(FILENAME).cpp -o $(FILENAME)
-	./$(FILENAME) -i example.txt -o output.txt
+	$(CXX) $(CXX_FLAGS) $(CXX_FLAGS2) $(FILENAME).cpp -o maze-solver
 run:
-	$(CXX) $(CFLAGS) $(FILENAME).cpp -o $(FILENAME)
+	$(CXX) $(CXX_FLAGS) $(FILENAME).cpp -o $(FILENAME)
 	./$(FILENAME) -i example.txt -o output.txt
 	rm $(FILENAME)
+debug:
+	$(CXX) $(CXX_FLAGS3) $(FILENAME).cpp -o $(FILENAME)
+clean:
+	rm maze-solver output.txt
